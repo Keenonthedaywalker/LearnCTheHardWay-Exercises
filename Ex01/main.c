3a25384d79cd1aa9f6cc7b76b1ee973995860606
Keenon Ribberink <keenon.ribberink@edu-kw1c.nl>
2025-07-17 10:37:06 +0200

    Completed the first two exercises(ex00 and ex01).

---
+++ Ex01/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* This is a comment */
+// This is the complete main function.
+// The first parameter is an integer that takes the argument count
+// The second parameter is an array of char *strings for the arguments themselves.
+int main(int argc, char *argv[])
+{
+
+    int distance = 100;
+
+    // This is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("Hopefully this is more complext than Hello World.\n");
+    printf("Can I take this higher?\n");
+    printf("Something very interesting is happening here.\n");
+    printf("123123123123\n");
+    printf("Test\n");
+
+}
